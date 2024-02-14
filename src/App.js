36a6cebd16d752fb443e15e21c29dc4f74d30a1e
R4V1-11 <ravithacker2003@gmail.com>
2024-02-14 15:04:28 +0530

    new UI for Login form further changes required

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import SignInForm from './Components/Login2';
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/login" exact element={<Login />} />
+          <Route path="/" exact element={<Login />} />
           <Route path="/signup" exact element={<Signup />} />
+          /*<Route path="/demo" exact element={<SignInForm />} />*/
         </Routes>
       </Router>
     </>
